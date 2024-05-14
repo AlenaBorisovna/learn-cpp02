d4077bd376b28751f972b56b70c1a85c4bc7cf5a
AlenaBorisovna <0549288kab@gmail.com>
2024-05-14 19:27:26 +0000

    new file main.cpp>

--- task-02/main.cpp
+++ task-02/main.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespase std;
+
+class Student {
+string name;
+string Female;
+int gruppa;
+int id;
+	public:
+string GetString() { return name()+Female() +to_string(gruppa)+to_string(id);}
+}
+class StudentsList:: public Student {
+
 int main() {}
